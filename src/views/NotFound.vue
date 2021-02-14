f2eb6a0ccd36a6ba79ffe3cc964343e2fb1107bd
kongyi <1269428345@qq.com>
2021-02-14 12:09:18 +0800

    add 404 页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    您访问的页面不存在
+    <div>
+      <router-link to="/">请返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
+
+<style scoped></style>
