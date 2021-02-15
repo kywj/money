c130e415b3310b9284b11e5678f603ead0871f59
kongyi <1269428345@qq.com>
2021-02-15 10:23:50 +0800

    add global style

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,13 @@ export default {
 </script>
 
 <style lang="scss">
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+@import "~@/assets/style/reset.scss";
+@import "~@/assets/style/helper.scss";
 body {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   line-height: 1.5;
-}
-a {
-  text-decoration: none;
-  color: inherit;
+  font-family: $font-hei;
+  color: #333;
 }
 </style>
