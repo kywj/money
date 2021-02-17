b3bf1bf49e459a3afe5e6dfd9409dcacd300b667
kongyi <1269428345@qq.com>
2021-02-17 15:08:39 +0800

    记录数据 2.0

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,6 @@
 <template>
   <layout class-prefix="layout">
-    {{ record }}
+    {{ recordList }}
     <Number-pad :value.sync="record.amount" @submit="saveRecord" />
     <Types :value.sync="record.type" />
     <Notes @update:value="onUpdateNotes" />
@@ -20,7 +20,8 @@ type Record = {
   tags: string[];
   notes: string;
   type: string;
-  amount: number;
+  amount: number; // 数据类型
+  createAt?: Date; // 类 / 构造函数
 };
 
 @Component({
@@ -28,8 +29,15 @@ type Record = {
 })
 export default class Money extends Vue {
   tags = ["衣", "食", "住", "行", "彩票"];
-  record: Record = { tags: [], notes: "", type: "-", amount: 0 };
-  recordList: Record[] = [];
+  record: Record = {
+    tags: [],
+    notes: "",
+    type: "-",
+    amount: 0,
+  };
+  recordList: Record[] = JSON.parse(
+    window.localStorage.getItem("recordList") || "[]"
+  );
 
   onUpdateTags(value: string[]) {
     this.record.tags = value;
@@ -38,9 +46,9 @@ export default class Money extends Vue {
     this.record.notes = value;
   }
   saveRecord() {
-    const record2 = JSON.parse(JSON.stringify(this.record));
+    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    record2.createAt = new Date();
     this.recordList.push(record2);
-    console.log(this.recordList);
   }
 
   @Watch("recordList")
