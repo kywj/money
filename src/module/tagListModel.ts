2f5f687998344522d3ee057e335b31aca6672b30
kongyi <1269428345@qq.com>
2021-02-18 16:53:27 +0800

    初始化 EditLabel

--- src/module/tagListModel.ts
+++ src/module/tagListModel.ts
@@ -1,8 +1,13 @@
 const localStorageKeyName = "recordList";
 
+type Tag = {
+  id: string;
+  name: string;
+};
+
 type TagListModel = {
-  data: string[];
-  fetch: () => string[];
+  data: Tag[];
+  fetch: () => Tag[];
   create: (name: string) => "成功" | "失败";
   save: () => void;
 };
@@ -16,10 +21,12 @@ const tagListModel: TagListModel = {
     return this.data;
   },
   create(name) {
-    if (this.data.indexOf(name) >= 0) {
+    // this.data = [{id: '1', name: '1'}, {id: '2', name: '2' }]
+    const names = this.data.map((item) => item.name);
+    if (names.indexOf(name) >= 0) {
       return "失败";
     }
-    this.data.push(name);
+    this.data.push({ id: name, name: name });
     this.save();
     return "成功";
   },
---
+++ src/views/EditLabel.vue
@@ -0,0 +1,13 @@
+<template>
+  <layout>编辑页面</layout>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class EditLabel extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
