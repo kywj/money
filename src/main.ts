e28421d7fb01f415c1225a88300bdf5de87126c6
kongyi <1269428345@qq.com>
2021-02-20 17:29:59 +0800

    封装 createTag

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,14 @@ Vue.component("Layout", Layout);
 Vue.component("Icon", Icon);
 
 window.tagList = tagListModel.fetch();
+window.createTag = (name: string) => {
+  const message = tagListModel.create(name);
+  if (message === "success") {
+    window.alert("添加成功");
+  } else if (message === "duplicated") {
+    window.alert("标签名重复");
+  }
+};
 
 new Vue({
   router,
