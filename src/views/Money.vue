40db253b95a8f83371eefbbde9683946e3883598
kongyi <1269428345@qq.com>
2021-02-21 12:32:19 +0800

    修复 money 组件 的新增标签bug

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -9,7 +9,7 @@
         @update:value="onUpdateNotes"
       />
     </div>
-    <Tags :data-source.sync="tags" @update:value="onUpdateTags" />
+    <Tags />
   </layout>
 </template>
 
@@ -26,7 +26,6 @@ import store from "@/store/index2.ts";
   components: { NumberPad, Types, FormItem, Tags },
 })
 export default class Money extends Vue {
-  tags = store.tagList;
   recordList = store.recordList;
   record: RecordItem = {
     tags: [],
@@ -34,10 +33,6 @@ export default class Money extends Vue {
     type: "-",
     amount: 0,
   };
-
-  onUpdateTags(value: string[]) {
-    this.record.tags = value;
-  }
   onUpdateNotes(value: string) {
     this.record.notes = value;
   }
