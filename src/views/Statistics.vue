6f3dcc2c4cad3753b4d24cf546605840d1c6c4d8
kongyi <1269428345@qq.com>
2021-02-24 13:13:18 +0800

    修复列表展示

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,14 +1,10 @@
 <template>
   <layout>
     <Tabs :dataSource="recordTypeList" class-prefix="type" :value.sync="type" />
-    <Tabs
-      :dataSource="intervalList"
-      class-prefix="interval"
-      :value.sync="interval"
-    />
+
     <ol>
-      <li v-for="(group, index) in result" :key="index.id">
-        <h3 class="title">{{ group.title }}</h3>
+      <li v-for="(group, index) in groupedList" :key="index">
+        <h3 class="title">{{ beautify(group.title) }}</h3>
         <ol>
           <li v-for="item in group.items" :key="item.id" class="record">
             <span>{{ tagString(item.tags) }}</span>
@@ -21,7 +17,94 @@
   </layout>
 </template>
 
-<style scoped lang="scss">
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import Tabs from "@/components/Tabs.vue";
+import intervalList from "@/constants/intervalList.ts";
+import recordTypeList from "@/constants/recordTypeList.ts";
+import fetchRecords from "@/store/index.ts";
+import dayjs from "dayjs";
+import clone from "@/lib/clone.ts";
+
+@Component({
+  components: { Tabs },
+})
+export default class Statistics extends Vue {
+  tagString(tags: Tag[]) {
+    return tags.length === 0 ? "无" : tags.join(",");
+  }
+  beautify(string: string) {
+    const day = dayjs(string);
+    const now = dayjs();
+    if (day.isSame(now, "day")) {
+      return "今天";
+    } else if (day.isSame(now.subtract(1, "day"), "day")) {
+      return "昨天";
+    } else if (day.isSame(now.subtract(2, "day"), "day")) {
+      return "前天";
+    } else if (day.isSame(now, "year")) {
+      return day.format("M月D日");
+    } else {
+      return day.format("YYYY年M月D日");
+    }
+  }
+  get recordList() {
+    return (this.$store.state as RootState).recordList;
+  }
+  get groupedList() {
+    const { recordList } = this;
+    if (recordList.length === 0) {
+      return [];
+    }
+    const newList = clone(recordList).sort((a, b) => {
+      dayjs(b.createAt).valueOf() - dayjs(a.createAt).valueOf();
+    });
+    const result = [
+      {
+        title: dayjs(newList[0].createAt).format("YYYY-MM-DD"),
+        items: [newList[0]],
+      },
+    ];
+    for (let i = 1; i < newList.length; i++) {
+      const current = newList[i];
+      const last = result[result.length - 1];
+      if (dayjs(last.title).isSame(dayjs(current.createAt), "day")) {
+        last.items.push(current);
+      } else {
+        result.push({
+          title: dayjs(current.createAt).format("YYYY-MM-DD"),
+          items: [current],
+        });
+      }
+    }
+    return result;
+  }
+  beforeCreate() {
+    this.$store.commit("fetchRecords");
+  }
+
+  type = "-";
+
+  recordTypeList = recordTypeList;
+}
+</script>
+
+<style lang="scss" scoped>
+::v-deep {
+  .type-tabs-item {
+    background: #c4c4c4;
+    &.selected {
+      background: white;
+      &.selected::after {
+        display: none;
+      }
+    }
+  }
+  .interval-tabs-item {
+    height: 48px;
+  }
+}
 %item {
   padding: 8px 16px;
   line-height: 24px;
@@ -40,64 +123,5 @@
   margin-right: auto;
   margin-left: 16px;
   color: #999;
-}
-</style>
-
-<script lang="ts">
-import Vue from "vue";
-import { Component } from "vue-property-decorator";
-import Tabs from "@/components/Tabs.vue";
-import intervalList from "@/constants/intervalList.ts";
-import recordTypeList from "@/constants/recordTypeList.ts";
-import fetchRecords from "@/store/index.ts";
-
-@Component({
-  components: { Tabs },
-})
-export default class Statistics extends Vue {
-  tagString(tags) {
-    return tags.length === 0 ? "无" : tags.join(",");
-  }
-  get recordList() {
-    return (this.$store.state as RootState).recordList;
-  }
-  get result() {
-    const { recordList } = this;
-    type HashTableValue = { title: string; items: RecordList[] };
-    const hashTable: {
-      [key: string]: HashTableValue;
-    } = {};
-    for (let i = 0; i < recordList.length; i++) {
-      const [date, time] = recordList[i].createAt!.split("T");
-      hashTable[date] = hashTable[date] || { title: date, items: [] };
-      hashTable[date].items.push(recordList[i]);
-    }
-    return hashTable;
-  }
-  beforeCreate() {
-    this.$store.commit("fetchRecords");
-  }
-
-  type = "-";
-  interval = "day";
-  intervalList = intervalList;
-  recordTypeList = recordTypeList;
-}
-</script>
-
-<style lang="scss" scoped>
-::v-deep {
-  .type-tabs-item {
-    background: white;
-    &.selected {
-      background: #c4c4c4;
-      &.selected::after {
-        display: none;
-      }
-    }
-  }
-  .interval-tabs-item {
-    height: 48px;
-  }
 }
 </style>
