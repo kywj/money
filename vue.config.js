f80d64a631b1c6f7b214e5165cffc25737d03670
kongyi <kywj1997@gmail.com>
2021-02-26 21:51:51 +0800

    部署代码到 morney-website

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/morney-website/'
+    : '/',
   lintOnSave: false,
   chainWebpack: config => {
     const dir = path.resolve(__dirname, 'src/assets/icons')
@@ -9,9 +12,9 @@ module.exports = {
       .rule('svg-sprite')
       .test(/\.svg$/)
       .include.add(dir).end() // 只包含 icons 目录
-      .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
-      .use('svgo-loader').loader('svgo-loader').tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
-    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
-    config.module.rule('svg').exclude.add(dir)     
+      .use('svg-sprite-loader').loader('svg-sprite-loader').options({ extract: false }).end()
+      .use('svgo-loader').loader('svgo-loader').tap(options => ({ ...options, plugins: [{ removeAttrs: { attrs: 'fill' } }] })).end()
+    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{ plainSprite: true }])
+    config.module.rule('svg').exclude.add(dir)
   }
 }
