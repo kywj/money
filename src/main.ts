f04462af10941b9e53043002618b912b7e387c58
kongyi <kywj1997@gmail.com>
2021-02-26 22:20:32 +0800

    添加二维码，优化用户体验

--- src/main.ts
+++ src/main.ts
@@ -24,3 +24,15 @@ window.onload = function () {
     window.scrollTo(0, 10000)
   }, 0)
 }
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert('使用二维码预览效果更佳')
+  const img = document.createElement('img')
+  img.src = '/qrcode.png'
+  img.style.position = 'fixed'
+  img.style.top = '50%'
+  img.style.left = '50%'
+  img.style.transform = 'translate(-50%, -50%)'
+  img.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.3)'
+  document.body.appendChild(img)
+}
