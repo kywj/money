fb95c5540bccfe345982536abdbd1fcd1308a367
ky <kywj1997@gmail.com>
2021-03-09 22:16:11 +0800

    构造数据

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -31,6 +31,8 @@ import recordTypeList from "@/constants/recordTypeList";
 import dayjs from "dayjs";
 import clone from "@/lib/clone";
 import Chart from "@/components/Chart.vue";
+import day from "dayjs";
+import _ from "lodash";
 
 @Component({
   components: { Tabs, Chart },
@@ -58,7 +60,30 @@ export default class Statistics extends Vue {
       return day.format("YYYY年M月D日");
     }
   }
+  get y() {
+    const today = new Date();
+    const array = [];
+    for (let i = 0; i <= 29; i++) {
+      const dateString = day(today).subtract(i, "day").format("YYYY-MM-DD");
+      const found = _.find(this.recordList, { createdAt: dateString });
+      array.push({ date: dateString, value: found ? found.amount : 0 });
+    }
+    array.sort((a, b) => {
+      if (a.date > b.date) {
+        return 1;
+      } else if ((a.date = b.date)) {
+        return 0;
+      } else {
+        return -1;
+      }
+    });
+    console.log(array);
+
+    return array;
+  }
   get x() {
+    const keys = this.y.map((item) => item.date);
+    const values = this.y.map((item) => item.value);
     return {
       grid: {
         left: 0,
@@ -68,38 +93,7 @@ export default class Statistics extends Vue {
         type: "category",
         axisTick: { alignWithLabel: true },
         axisLine: { lineStyle: { color: "#666" } },
-        data: [
-          "1",
-          "2",
-          "3",
-          "4",
-          "5",
-          "6",
-          "7",
-          "8",
-          "9",
-          "10",
-          "11",
-          "12",
-          "13",
-          "14",
-          "15",
-          "16",
-          "17",
-          "18",
-          "19",
-          "20",
-          "21",
-          "22",
-          "23",
-          "24",
-          "25",
-          "26",
-          "27",
-          "28",
-          "29",
-          "30",
-        ],
+        data: keys,
       },
       yAxis: {
         type: "value",
@@ -110,38 +104,7 @@ export default class Statistics extends Vue {
           symbol: "circle",
           itemStyle: { borderWidth: 1, color: "#666", borderColor: "#666" },
           symbolSize: 12,
-          data: [
-            150,
-            230,
-            224,
-            218,
-            135,
-            147,
-            260,
-            150,
-            230,
-            224,
-            218,
-            135,
-            147,
-            260,
-            150,
-            230,
-            224,
-            218,
-            135,
-            147,
-            260,
-            150,
-            230,
-            224,
-            218,
-            135,
-            147,
-            260,
-            1,
-            2,
-          ],
+          data: values,
           type: "line",
         },
       ],
